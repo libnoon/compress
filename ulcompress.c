4a65668ccb74368fdcad8f0c2371ce9855ccfc22
Fabrice Bauzac <libnoon@gmail.com>
2019-10-16 22:50:51 +0200

    Update copyright years

--- ulcompress.c
+++ ulcompress.c
@@ -1,6 +1,6 @@
 /*
   Compress - file compressor and decompressor.
-  Copyright (C) 2005  Fabrice Bauzac
+  Copyright (C) 2005, 2019  Fabrice Bauzac
 
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
